e8fe1fdb30eedc02d565114697b3df4211dbc5e1
Robin Wieruch <wrobin@gmx.net>
2019-04-24 15:00:44 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -13,18 +13,51 @@ const initialTodos = [
   },
 ];
 
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-  const handleChange = () => {};
+  const [todos, dispatch] = React.useReducer(
+    todoReducer,
+    initialTodos
+  );
+
+  const handleChange = todo => {
+    dispatch({
+      type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+      id: todo.id,
+    });
+  };
 
   return (
     <ul>
-      {initialTodos.map(todo => (
+      {todos.map(todo => (
         <li key={todo.id}>
           <label>
             <input
               type="checkbox"
               checked={todo.complete}
-              onChange={handleChange}
+              onChange={() => handleChange(todo)}
             />
             {todo.task}
           </label>
